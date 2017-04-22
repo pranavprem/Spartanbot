63608b06b9aaef3352aaaec9e15ded10d96a6bfa
Pranav Prem <pranavprem93@gmail.com>
2017-04-22 10:24:43 -0700

    edited integration details

---
+++ findbotid.py
@@ -0,0 +1,19 @@
+import os
+from slackclient import SlackClient
+
+
+BOT_NAME = 'testonebot'
+
+slack_client = SlackClient('xoxb-158482568483-SBwFgJ50Vxz92d34X0UzB4Hj')
+
+print "something"
+if __name__ == "__main__":
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
