84a121cead27141b6a57186784e57111c2d2f727
foram mehta <forammehta90@gmail.com>
2017-04-27 16:11:46 -0700

    Resolved reference issues

--- sentence-engine.py
+++ sentence-engine.py
@@ -5,23 +5,23 @@ from PyDictionary import PyDictionary
 class sentence_engine(object):
 
     def __init__(self):
-        dbclient = db.spartandb()
-        dictionary = PyDictionary()
+        self.dbclient = db.spartandb()
+        self.dictionary = PyDictionary()
         
 
     def analyze(self, sentence):
         ret_dict={}
-        pre_objects = dbclient.get_objects()
-        pre_subjects = dbclient.get_subjects()
+        pre_objects = self.dbclient.get_objects()
+        pre_subjects = self.dbclient.get_subjects()
         objects = []
         subjects = []
         for word in sentence.split(" "):
             if word in pre_objects:
                 objects.append(word)
-                objects.append(dictionary.synonym(word))
+                objects.append(self.dictionary.synonym(word))
             if word in pre_subjects:
                 subjects.append(word)
-                subjects.append(dictionary.synonym(word))
+                subjects.append(self.dictionary.synonym(word))
         
         ret_dict["objects"] = objects
         ret_dict["subjects"] = subjects
