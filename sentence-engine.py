86b044222b1f0f8a0b38fbdc7c5851ef08d96763
foram mehta <forammehta90@gmail.com>
2017-04-28 19:47:52 -0700

    Object subject changed to keywords

--- sentence-engine.py
+++ sentence-engine.py
@@ -11,20 +11,14 @@ class sentence_engine(object):
 
     def analyze(self, sentence):
         ret_dict={}
-        pre_objects = self.dbclient.get_objects()
-        pre_subjects = self.dbclient.get_subjects()
-        objects = []
-        subjects = []
+        pre_keywords = self.dbclient.get_keywords()
+        keywords = []
         for word in sentence.split(" "):
-            if word in pre_objects:
-                objects.append(word)
-                objects.append(self.dictionary.synonym(word))
-            if word in pre_subjects:
-                subjects.append(word)
-                subjects.append(self.dictionary.synonym(word))
-        
-        ret_dict["objects"] = objects
-        ret_dict["subjects"] = subjects
+            if word in pre_keywords:
+                keywords.append(word)
+                keywords.append(self.dictionary.synonym(word))
+            
+        ret_dict["keywords"] = keywords
         ret_dict["sentence"] = sentence
         return ret_dict 
                         
