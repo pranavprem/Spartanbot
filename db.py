20f198f63140ca704c664eeae257500fbdf82895
Pranav Prem <pranavprem93@gmail.com>
2017-04-28 19:05:28 -0700

    Fixed inserting objects and subjects. Changed architecture from subjecs and objects to only keywords

--- db.py
+++ db.py
@@ -10,24 +10,28 @@ class spartandb(object):
     def __init__(self):
         self.client = MongoClient(os.environ["MONGODB_URI"])
         self.db = self.client.SpartanData     
+    
     def insert(self,data):
         self.db.greensheet.insert_one(data)
+    
     def read(self):
         ret_obj=[]
         for obj in self.db.greensheet.find():
             ret_obj.append(obj)
         return ret_obj
-    def insert_subjects(self,data):
-        self.db.subjects.insert_one(data)
-    def get_subjects(self):
-        ret_sub = []
-        for sub in self.db.greensheet.find():
-            ret_sub.append(sub)
-        return ret_sub
-    def insert_objects(self,data):
-        self.db.obj.insert_one(data)
-    def get_objects(self):
-        ret_ob=[]
-        for ob in self.db.greensheet.find():
-            ret_ob.append(ob)
-        return ret_ob
+    
+    def insert_keyword(self,keyword):
+        try:
+            key_array = self.db.keywords.find().next()
+            key_array["keywords"].append(keyword)
+            self.db.keywords.save(key_array)
+        except StopIteration:
+            key_array=[]
+            key_array.append(keyword)
+            self.db.keywords.insert_one({"keywords":key_array})
+
+    def get_keywords(self):
+        keywords = []
+        for obj in self.db.keywords.find():
+            keywords.append(obj["keywords"])
+        return keywords
