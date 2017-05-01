0c455263edeea9bc211a8539447d709412dcfc11
foram mehta <forammehta90@gmail.com>
2017-05-01 14:58:32 -0700

    Handling of null case.

--- sentence_engine.py
+++ sentence_engine.py
@@ -11,19 +11,20 @@ class sentence_engine(object):
 
     def analyze(self, sentence):
         ret_dict={}
-        #lowercase_sen = sentence.lower()
+        lowercase_sen = sentence.lower()
         pre_subjects = self.dbclient.get_subjects()
         pre_objects = self.dbclient.get_objects()
-        subjects = []
-        objects = []
-        for word in sentence.split(" "):
-            if word.lower() in pre_subjects:
-                subjects.append(word)
-            if word.lower() in pre_objects:
-                objects.append(self.dictionary.synonym(word))
+        keywords = []
+        for word in lowercase_sen.split(" "):
+            if word in pre_subjects:
+                keywords.append(word)
+            if word in pre_objects:
+                if self.dictionary.synonym(word):
+                    for synonyms in self.dictionary.synonym(word):
+                        keywords.append(synonyms.lower())
+                
             
-        ret_dict["subjects"] = subjects
-        ret_dict["objects"] = objects
+        ret_dict["keywords"] = keywords
         ret_dict["sentence"] = sentence
         return ret_dict 
                         
