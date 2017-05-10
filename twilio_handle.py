f6854794607f98c507e1ce7dab700049401d1e4c
Pranav Prem <pranavprem93@gmail.com>
2017-05-09 22:00:44 -0700

    twilio client testing reply

--- twilio_handle.py
+++ twilio_handle.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask, request
 # from twilio import twiml
-# from twilio.rest import TwilioRestClient
+from twilio.rest import TwilioRestClient
 from slackclient import SlackClient
 
 AT_BOT = "<@" + os.environ["bot_id"] + ">"
 twilio_number = os.environ.get("twilio_number")
 accept_number = os.environ.get("user_number")
 app = Flask(__name__)
-# twilio_client = TwilioRestClient()
+twilio_client = TwilioRestClient()
 slack_client = SlackClient(os.environ.get("slacktoken", None))
  
 @app.route("/twilio", methods=['POST'])
@@ -20,6 +20,8 @@ def post_to_slack():
         slack_client.api_call("chat.postMessage",
                               channel="#testonebotchanel",
                               text=AT_BOT +" "+ message, as_user=True)
+        twilio_client.messages.create(to=accept_number, from_=twilio_number,
+                                      body="response")
     # return Response(response.toxml(), mimetype="text/xml"), 200
     return '', 200
 
